4991647188924bca14bef437249e58e304d74bb6
Tran Long <long.pham.tran2612@gmail.com>
2022-08-21 16:23:41 +0700

    nextauth with github

--- components/narbar.js
+++ components/narbar.js
@@ -1,14 +1,14 @@
 import Link from 'next/link'
-import { signIn, signOut, useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Navbar() {
-//   const [session, loading] = useSession()
+    const { data: session } = useSession()
   return (
     <nav className='header'>
       <h1 className='logo'>
         <a href='#'>NextAuth</a>
       </h1>
-      <ul className={`main-nav`}>
+      <ul className={`main-nav ${!session}`}>
         <li>
           <Link href='/'>
             <a>Home</a>
@@ -24,6 +24,32 @@ function Navbar() {
             <a>Blog</a>
           </Link>
         </li>
+        {!session && (
+          <li>
+            <Link href='/api/auth/signin'>
+              <a
+                onClick={e => {
+                  e.preventDefault()
+                  signIn('github')
+                }}>
+                Sign In
+              </a>
+            </Link>
+          </li>
+        )}
+        {session && (
+          <li>
+            <Link href='/api/auth/signout'>
+              <a
+                onClick={e => {
+                  e.preventDefault()
+                  signOut()
+                }}>
+                Sign Out
+              </a>
+            </Link>
+          </li>
+        )}
 
       </ul>
     </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from '../components/layout/header'
 import Footer from '../components/layout/footer'
 import Navbar from 'components/narbar'
+import 'components/narbar.css'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component,  pageProps: { session, ...pageProps } }) {
   if(Component.getLayout) {
     return Component.getLayout(<Component {...pageProps} />)
   }
 
   return <>
+   <SessionProvider session={session}>
   <Navbar/>
   <Component {...pageProps} />
   <Footer/>
+  </SessionProvider>
   </> 
 }
 
